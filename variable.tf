7fb59ebdbef5ba3e75926f40c8b6f2c8f8a52bd3
owner <kakuma3090@gmail.com>
2026-01-19 20:36:26 +0900

    refactor: Update variables for GCP

--- variable.tf
+++ variable.tf
@@ -1,15 +1,22 @@
+variable "project_id" {
+  description = "The GCP project ID to deploy resources to."
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region to deploy resources to."
+  type        = string
+  default     = "asia-northeast1"
+}
+
 variable "domain_name" {
   description = "The main domain name for the application."
   type        = string
   default     = "example.com"
 }
 
-variable "region" {
-  type = string
-  default = "ap-northeast-1"
-}
-
 variable "bucket_name" {
-  type = string
-  default = "sample-bucket-name"
+  description = "The name for the GCS bucket."
+  type        = string
+  default     = "sample-gcs-bucket-name"
 }
