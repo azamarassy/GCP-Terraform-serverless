c6d392f085ac26c5031ac6f62c841a94a65fbf9b
owner <kakuma3090@gmail.com>
2026-01-19 20:41:37 +0900

    feat(frontend): Add GCP CDN and HTTP Load Balancer

--- frontend.tf
+++ frontend.tf
@@ -95,3 +95,37 @@ resource "aws_route53_record" "root" {
     evaluate_target_health = false
   }
 }
+
+# --- GCP CDN and Load Balancer ---
+
+# CDN用のグローバルIPアドレスを予約
+resource "google_compute_global_address" "cdn_ip" {
+  name = "cdn-ip"
+}
+
+# GCSバケットをバックエンドとして設定
+resource "google_compute_backend_bucket" "frontend_backend" {
+  name        = "frontend-backend-bucket"
+  bucket_name = google_storage_bucket.frontend.name
+  enable_cdn  = true
+}
+
+# URLマップを作成
+resource "google_compute_url_map" "url_map" {
+  name            = "url-map"
+  default_service = google_compute_backend_bucket.frontend_backend.id
+}
+
+# HTTPプロキシを作成
+resource "google_compute_target_http_proxy" "http_proxy" {
+  name    = "http-proxy"
+  url_map = google_compute_url_map.url_map.id
+}
+
+# 転送ルールを作成
+resource "google_compute_global_forwarding_rule" "http_forwarding_rule" {
+  name       = "http-forwarding-rule"
+  target     = google_compute_target_http_proxy.http_proxy.id
+  port_range = "80"
+  ip_address = google_compute_global_address.cdn_ip.address
+}
