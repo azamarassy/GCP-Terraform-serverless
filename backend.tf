50ba2b709074dbaa8c2dc782ba3a2f3316d07b2a
azamarassy <osanatsuchi@gmail.com>
2026-01-22 17:47:50 +0900

    docs: add comments to backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,8 @@
+# Terraformのバックエンド設定
 terraform {
+  # Terraformの状態ファイル（.tfstate）をGCSバケットに保存するように設定
   backend "gcs" {
-    bucket  = "terraform-state-bucket-gcp"
-    prefix  = "terraform/state"
+    bucket  = "terraform-state-bucket-gcp" # 状態ファイルを保存するGCSバケットの名前
+    prefix  = "terraform/state"            # バケット内の状態ファイルのパス（プレフィックス）
   }
 }
